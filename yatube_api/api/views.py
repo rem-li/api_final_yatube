71fcc0743a3fc1a28289b4df16f6358125ba34a4
Елизавета Веремьёва <eliza.remli@gmail.com>
2022-11-29 21:54:28 +0300

    версия 1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, filters
-from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from posts.models import Post, Group, Follow
 from .serializers import (CommentSerializer, PostSerializer, GroupSerializer,
